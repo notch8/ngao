c23bdb7bd8e580e546d572543be85271b39d1d11
Daniel Lim <daniel@notch8.com>
2020-04-07 08:45:16 -0700

    remove user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
