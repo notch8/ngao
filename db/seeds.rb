db2c5d036bf839a368f6545dc6970cd68ba2dd79
Daniel Lim <daniel@notch8.com>
2020-04-07 13:40:39 -0700

    added users for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.where(email: 'jlhardes@iu.edu').first_or_create do |u|
+  u.password = 'testing123'
+end
+
+User.where(email: 'rdfloyd@iu.edu').first_or_create do |u|
+  u.password = 'testing123'
+end
+
+User.where(email: 'acrande@iu.edu').first_or_create do |u|
+  u.password = 'testing123'
+end
