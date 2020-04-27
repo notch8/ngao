82ac6960e1915ca5c3959f05988e68058d632938
Daniel Lim <daniel@notch8.com>
2020-04-27 10:29:53 -0700

    added invite button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :invitable,
          :recoverable, :rememberable, :validatable
 
   enum role: { manager: 0, admin: 1 }
