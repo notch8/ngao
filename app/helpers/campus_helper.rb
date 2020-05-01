776a7ce01762a009b3d34494a5e9fd44e0183650
Lea Ann Bradford <leaann@notch8.com>
2020-05-01 08:33:12 -0700

    add back default campust (#57)

--- app/helpers/campus_helper.rb
+++ app/helpers/campus_helper.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
 
 module CampusHelper
   include ActiveSupport::Concern
 
   CAMPUSES = {
-    "US-InU" => "Indiana University Bloomington",
-    "US-InU-Sb" => "Indiana University South Bend",
-    "US-InU-Se" => "Indiana University Southeast",
-    "US-inrmiue" => "Indiana University East",
-    "US-InU-K" => "Indiana University Kokomo",
-    "US-iniu" => "Indiana University-Purdue University, Indianapolis",
-    "US-InU-N" => "Indiana University Northwest"
+    'US-InU' => 'Indiana University Bloomington',
+    'US-InU-Sb' => 'Indiana University South Bend',
+    'US-InU-Se' => 'Indiana University Southeast',
+    'US-inrmiue' => 'Indiana University East',
+    'US-InU-K' => 'Indiana University Kokomo',
+    'US-iniu' => 'Indiana University-Purdue University, Indianapolis',
+    'US-InU-N' => 'Indiana University Northwest'
   }
 
-  def convert_campus_id value
-    return CAMPUSES[value]
+  def convert_campus_id(value)
+    CAMPUSES[value] || 'Indiana University'
   end
-  
 end
