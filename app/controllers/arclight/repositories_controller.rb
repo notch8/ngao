84846e797fa40b85fbff0816c4819e7126572c63
Randall Floyd <rdfloyd@indiana.edu>
2020-07-15 10:16:18 -0400

    AR-64 Adding Solr parameter to sort collections by title (#86)

--- app/controllers/arclight/repositories_controller.rb
+++ app/controllers/arclight/repositories_controller.rb
@@ -17,7 +17,8 @@ module Arclight
       search_service = Blacklight.repository_class.new(blacklight_config)
       @response = search_service.search(
         q: "level_sim:Collection repository_sim:\"#{@repository.name}\"",
-        rows: 100
+        rows: 100,
+        sort: 'title_sort asc'
       )
       @collections = @response.documents
     end
