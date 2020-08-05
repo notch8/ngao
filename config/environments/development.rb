b6b9aca70b3c591f950f67e277ebee29b308a9f6
Randall Floyd <rdfloyd@indiana.edu>
2020-08-05 15:51:14 -0400

    Correcting several things found wrong while deploying server environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+
+  # We have to cache classes with Delayed Job or it will peg the CPU reloading classes every 5 seconds when
+  # it checks for jobs.
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = false
