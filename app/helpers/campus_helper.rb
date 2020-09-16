7ce325aae66b0a82303ae2fdbe12ff95a0b55199
Julie Hardesty <jlhardes@iu.edu>
2020-09-16 17:01:38 -0400

    Changed IUPUI campus name to IUPUI (#109)

--- app/helpers/campus_helper.rb
+++ app/helpers/campus_helper.rb
@@ -9,7 +9,7 @@ module CampusHelper
     'US-InU-Se' => 'Indiana University Southeast',
     'US-inrmiue' => 'Indiana University East',
     'US-InU-K' => 'Indiana University Kokomo',
-    'US-iniu' => 'Indiana University-Purdue University, Indianapolis',
+    'US-iniu' => 'IUPUI',
     'US-InU-N' => 'Indiana University Northwest'
   }
 
